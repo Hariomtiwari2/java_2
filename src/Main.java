481eb1e35eb91a79fe1acfb4f3c538c78cd257ed
Hariom Tiwari <hariomtiwari20004@gmail.com>
2026-05-24 15:07:27 +0530

    some abstract method pracctice

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("How are you !!!");
+        Car c1 = new BMW();
+        System.out.println(c1+" has "+c1.tyers );
+        c1.brake();
+    }
+}
+
+abstract class Car {
+    int tyers ;
+
+    public Car(int tyers) {
+        this.tyers = tyers;
+    }
+    abstract void brake();
+}
+
+class BMW extends Car{
+
+
+    public BMW() {
+        super(4);
+    }
+
+    @Override
+    void brake() {
+        System.out.println("very good break");
     }
 }
