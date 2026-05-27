7cc199ddf3d6f8644440afd767542d7bcb81ac8c
Hariom Tiwari <hariomtiwari20004@gmail.com>
2026-05-27 21:27:23 +0530

    starting to study multithreading in java

---
+++ src/multithreading/MAin.java
@@ -0,0 +1,10 @@
+package multithreading;
+import java.lang.Thread;
+
+ // java supports multithreadong through java.lang.thread class & java.lang.runnable interface
+public class MAin {
+    public static void main(String[] args) {
+
+        System.out.println(Thread.currentThread().getName());
+    }
+}
